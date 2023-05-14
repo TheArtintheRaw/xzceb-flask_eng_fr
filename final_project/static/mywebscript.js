6829bd20439e70e20612ba24cc5a2368a53ae96b
Roger <theartintheraw@gmail.com>
2023-05-14 14:17:02 -0400

    .

--- final_project/static/mywebscript.js
+++ final_project/static/mywebscript.js
@@ -4,10 +4,10 @@ let translateToFrench = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            document.getElementById("translatedText").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "englishToFrench?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "englishToFrench?english_text"+"="+textToTranslate, true);
     xhttp.send();
 }
 
@@ -17,10 +17,10 @@ let translateToEnglish = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            document.getElementById("translatedText").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "frenchToEnglish?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "frenchToEnglish?french_text"+"="+textToTranslate, true);
     xhttp.send();
 }
 
