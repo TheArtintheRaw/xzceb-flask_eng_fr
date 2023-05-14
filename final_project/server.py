4bdc8c43fa83a5a532092c602ccd633e00cf6bdb
Roger <theartintheraw@gmail.com>
2023-05-14 13:17:47 -0400

    .

--- final_project/server.py
+++ final_project/server.py
@@ -6,13 +6,13 @@ app = Flask("Web Translator")
 
 @app.route('/englishToFrench', methods=['GET', 'POST'])
 def english_to_french():    
-    english_text = request.form.get('textToTranslate')  # replace 'text' with the actual name of your input field
+    english_text = request.form.get('text')  # replace 'text' with the actual name of your input field
     french_text = translator.english_to_french(english_text)
     return french_text
 
 @app.route('/frenchToEnglish', methods=['GET', 'POST'])
 def french_to_english():    
-    french_text = request.form.get('textToTranslate')  # replace 'text' with the actual name of your input field
+    french_text = request.form.get('text')  # replace 'text' with the actual name of your input field
     english_text = translator.french_to_english(french_text)
     return english_text
 
